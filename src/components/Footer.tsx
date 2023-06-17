a94c89d6c29d61f1001cf56143374b0d1f067b30
Victor Turra <victor.florencio@univem.edu.br>
2023-06-17 13:33:34 -0300

    chore: add payments section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,6 +12,16 @@ import {
   IconPhone,
 } from "@tabler/icons-react";
 
+// Import payment icons
+import AmexLightLarge from "@/assets/payments/amex-light-large.svg";
+import BtcLightLarge from "@/assets/payments/btc-light-large.svg";
+import CirrusLightLarge from "@/assets/payments/cirrus-light-large.svg";
+import DinnerClubLightLarge from "@/assets/payments/dinnerclub-light-large.svg";
+import MaestroLightLarge from "@/assets/payments/maestro-light-large.svg";
+import MastercardLightLarge from "@/assets/payments/mastercard-light-large.svg";
+import PaypalLightLarge from "@/assets/payments/paypal-light-large.svg";
+import VisaLightLarge from "@/assets/payments/visa-light-large.svg";
+
 function Footer() {
   return (
     <MFooter className="p--container" height={100}>
@@ -106,9 +116,102 @@ function Footer() {
       <Section>
         <Title order={4}>Institucional</Title>
         <List>
-          <ListItem><Anchor>Sobre Nós</Anchor></ListItem>
+          <ListItem>
+            <Anchor>Sobre Nós</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Missão e Visão</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Valores</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Equipe</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Parceiros</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Políticas de Privacidade</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Termos e Condições</Anchor>
+          </ListItem>
+          <ListItem>
+            <Anchor>Contato</Anchor>
+          </ListItem>
         </List>
       </Section>
+      <Section>
+        <Title order={4}>Formas de pagamento</Title>
+        <List>
+          <ListItem>
+            <Image
+              src={AmexLightLarge}
+              alt="Amex"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={BtcLightLarge}
+              alt="BTC"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={CirrusLightLarge}
+              alt="Cirrus"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={DinnerClubLightLarge}
+              alt="Dinner Club"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={MaestroLightLarge}
+              alt="Maestro"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={MastercardLightLarge}
+              alt="Mastercard"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={PaypalLightLarge}
+              alt="Paypal"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+          <ListItem>
+            <Image
+              src={VisaLightLarge}
+              alt="Visa"
+              width={48}
+              height={48}
+            />
+          </ListItem>
+        </List>
+      </Section>
+      
     </MFooter>
   );
 }
